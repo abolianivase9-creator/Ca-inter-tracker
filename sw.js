0bb4a5f236aa4a5f106519571d7f07e0b53778fa
abolianivase9-creator <abolianivase9@gmail.com>
2025-11-29 21:21:16 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ca-tracker-cache-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((r) => r || fetch(evt.request))
+  );
+});
